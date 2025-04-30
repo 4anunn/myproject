8ed41af274511a8aa63b7091a7c31aed20d5b8c6
4anunn <anujin5822@gmail.com>
2025-04-30 12:16:29 +0800

    Add files via upload

---
+++ soril2_1/soril2_1/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace soril2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnTake7 = new System.Windows.Forms.Button();
+            this.btnTake6 = new System.Windows.Forms.Button();
+            this.btnTake5 = new System.Windows.Forms.Button();
+            this.btnTake4 = new System.Windows.Forms.Button();
+            this.btnTake3 = new System.Windows.Forms.Button();
+            this.btnTake2 = new System.Windows.Forms.Button();
+            this.btnTake1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblRemainingDice = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("News706 BT", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 45);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "DICE GAME 2025";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(63, 670);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(157, 57);
+            this.btnRestart.TabIndex = 14;
+            this.btnRestart.Text = "Дахин эхлэх";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(63, 474);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(246, 170);
+            this.txtLog.TabIndex = 13;
+            this.txtLog.Text = "";
+            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnTake7);
+            this.groupBox3.Controls.Add(this.btnTake6);
+            this.groupBox3.Controls.Add(this.btnTake5);
+            this.groupBox3.Controls.Add(this.btnTake4);
+            this.groupBox3.Controls.Add(this.btnTake3);
+            this.groupBox3.Controls.Add(this.btnTake2);
+            this.groupBox3.Controls.Add(this.btnTake1);
+            this.groupBox3.Location = new System.Drawing.Point(63, 311);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(536, 121);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Та хэдэн шоо авах вэ?";
+            // 
+            // btnTake7
+            // 
+            this.btnTake7.Location = new System.Drawing.Point(452, 34);
+            this.btnTake7.Name = "btnTake7";
+            this.btnTake7.Size = new System.Drawing.Size(58, 56);
+            this.btnTake7.TabIndex = 6;
+            this.btnTake7.Text = "7";
+            this.btnTake7.UseVisualStyleBackColor = true;
+            this.btnTake7.Click += new System.EventHandler(this.btnTake7_Click);
+            // 
+            // btnTake6
+            // 
+            this.btnTake6.Location = new System.Drawing.Point(379, 34);
+            this.btnTake6.Name = "btnTake6";
+            this.btnTake6.Size = new System.Drawing.Size(58, 56);
+            this.btnTake6.TabIndex = 5;
+            this.btnTake6.Text = "6";
+            this.btnTake6.UseVisualStyleBackColor = true;
+            this.btnTake6.Click += new System.EventHandler(this.btnTake6_Click);
+            // 
+            // btnTake5
+            // 
+            this.btnTake5.Location = new System.Drawing.Point(306, 34);
+            this.btnTake5.Name = "btnTake5";
+            this.btnTake5.Size = new System.Drawing.Size(58, 56);
+            this.btnTake5.TabIndex = 4;
+            this.btnTake5.Text = "5";
+            this.btnTake5.UseVisualStyleBackColor = true;
+            this.btnTake5.Click += new System.EventHandler(this.btnTake5_Click);
+            // 
+            // btnTake4
+            // 
+            this.btnTake4.Location = new System.Drawing.Point(233, 34);
+            this.btnTake4.Name = "btnTake4";
+            this.btnTake4.Size = new System.Drawing.Size(58, 56);
+            this.btnTake4.TabIndex = 3;
+            this.btnTake4.Text = "4";
+            this.btnTake4.UseVisualStyleBackColor = true;
+            this.btnTake4.Click += new System.EventHandler(this.btnTake4_Click);
+            // 
+            // btnTake3
+            // 
+            this.btnTake3.Location = new System.Drawing.Point(160, 34);
+            this.btnTake3.Name = "btnTake3";
+            this.btnTake3.Size = new System.Drawing.Size(58, 56);
+            this.btnTake3.TabIndex = 2;
+            this.btnTake3.Text = "3";
+            this.btnTake3.UseVisualStyleBackColor = true;
+            this.btnTake3.Click += new System.EventHandler(this.btnTake3_Click);
+            // 
+            // btnTake2
+            // 
+            this.btnTake2.Location = new System.Drawing.Point(90, 34);
+            this.btnTake2.Name = "btnTake2";
+            this.btnTake2.Size = new System.Drawing.Size(58, 56);
+            this.btnTake2.TabIndex = 1;
+            this.btnTake2.Text = "2";
+            this.btnTake2.UseVisualStyleBackColor = true;
+            this.btnTake2.Click += new System.EventHandler(this.btnTake2_Click);
+            // 
+            // btnTake1
+            // 
+            this.btnTake1.Location = new System.Drawing.Point(17, 34);
+            this.btnTake1.Name = "btnTake1";
+            this.btnTake1.Size = new System.Drawing.Size(58, 56);
+            this.btnTake1.TabIndex = 0;
+            this.btnTake1.Text = "1";
+            this.btnTake1.UseVisualStyleBackColor = true;
+            this.btnTake1.Click += new System.EventHandler(this.btnTake1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTurn);
+            this.groupBox2.Location = new System.Drawing.Point(360, 184);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 85);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Тоглогчийн ээлж";
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Location = new System.Drawing.Point(71, 40);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(44, 16);
+            this.lblTurn.TabIndex = 1;
+            this.lblTurn.Text = "label2";
+            this.lblTurn.Click += new System.EventHandler(this.lblTurn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblRemainingDice);
+            this.groupBox1.Location = new System.Drawing.Point(63, 184);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 85);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Үлдсэн шоо";
+            // 
+            // lblRemainingDice
+            // 
+            this.lblRemainingDice.AutoSize = true;
+            this.lblRemainingDice.Location = new System.Drawing.Point(71, 40);
+            this.lblRemainingDice.Name = "lblRemainingDice";
+            this.lblRemainingDice.Size = new System.Drawing.Size(44, 16);
+            this.lblRemainingDice.TabIndex = 1;
+            this.lblRemainingDice.Text = "label2";
+            this.lblRemainingDice.Click += new System.EventHandler(this.lblRemainingDice_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(352, 474);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(247, 170);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "Hint";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pictureBox1.Image = global::soril2_1.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(468, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 131);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(663, 796);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnTake7;
+        private System.Windows.Forms.Button btnTake6;
+        private System.Windows.Forms.Button btnTake5;
+        private System.Windows.Forms.Button btnTake4;
+        private System.Windows.Forms.Button btnTake3;
+        private System.Windows.Forms.Button btnTake2;
+        private System.Windows.Forms.Button btnTake1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblRemainingDice;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ soril2_1/soril2_1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace soril2_1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=ADMIN\\SQLEXPRESS; Initial Catalog=Shagai; Integrated Security=True;";
+        int remainingDice = 2025;
+        bool isPlayer1Turn = true;
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            remainingDice = 30;
+            isPlayer1Turn = true;
+            txtLog.Clear();
+            Log("Тоглоом эхэллээ. Эхний шоо авалт...");
+            PlayerMove(1); // 1-р тоглогч үргэлж 1 шоо авна
+        }
+
+        private void PlayerMove(int numDice)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            if (remainingDice <= 0) return;
+
+            remainingDice -= numDice;
+            Log($"1-р тоглогч {numDice} шоо авлаа. Үлдсэн: {remainingDice}");
+
+            CheckForWin();
+            string query = "Insert Into ShagaiGame (playerName, taken, [left]) values (@playerName, @taken, @left)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@playerName", "1-r toglogch");
+            cmd.Parameters.AddWithValue("@taken", numDice);
+            cmd.Parameters.AddWithValue("left", remainingDice);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            if (remainingDice > 0)
+            {
+                isPlayer1Turn = false;
+                UpdateUI();
+                ComputerMove();
+            }
+        }
+
+        private void ComputerMove()
+        {
+            int maxTake = Math.Min(7, remainingDice);
+            int compTake = rand.Next(1, maxTake + 1);
+
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            remainingDice -= compTake;
+            Log($"2-р тоглогч {compTake} шоо авлаа. Үлдсэн: {remainingDice}");
+
+            string query = "Insert Into ShagaiGame (playerName, taken, [left]) values (@playerName, @taken, @left)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@playerName", "2-r toglogch");
+            cmd.Parameters.AddWithValue("@taken", compTake);
+            cmd.Parameters.AddWithValue("left", remainingDice);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (remainingDice > 7)
+            {
+                richTextBox1.Text = $"8 - {compTake} = {8 - compTake}";
+            }
+            else
+            {
+                richTextBox1.Text = $"{compTake}";
+            }
+
+            CheckForWin();
+
+            if (remainingDice > 0)
+            {
+                isPlayer1Turn = true;
+                UpdateUI();
+            }
+        }
+
+
+        private void CheckForWin()
+        {
+            if (remainingDice <= 0)
+            {
+                string winner = isPlayer1Turn ? "1-р тоглогч" : "2-р тоглогч";
+                Log($"{winner} хожлоо!");
+                DisableButtons();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            lblRemainingDice.Text = $"Үлдсэн шоо: {remainingDice}";
+            lblTurn.Text = isPlayer1Turn ? "1-р тоглогчийн ээлж" : "2-р тоглогчийн ээлж";
+        }
+
+        private void Log(string message)
+        {
+            txtLog.AppendText(message + Environment.NewLine);
+        }
+
+
+
+        private void DisableButtons()
+        {
+            btnTake1.Enabled = false;
+            btnTake2.Enabled = false;
+            btnTake3.Enabled = false;
+            btnTake4.Enabled = false;
+            btnTake5.Enabled = false;
+            btnTake6.Enabled = false;
+            btnTake7.Enabled = false;
+        }
+
+        private void EnableButtons()
+        {
+            btnTake1.Enabled = true;
+            btnTake2.Enabled = true;
+            btnTake3.Enabled = true;
+            btnTake4.Enabled = true;
+            btnTake5.Enabled = true;
+            btnTake6.Enabled = true;
+            btnTake7.Enabled = true;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            EnableButtons();
+            StartGame();
+        }
+
+        private void btnTake1_Click(object sender, EventArgs e) => PlayerMove(1);
+        private void btnTake2_Click(object sender, EventArgs e) => PlayerMove(2);
+        private void btnTake3_Click(object sender, EventArgs e) => PlayerMove(3);
+        private void btnTake4_Click(object sender, EventArgs e) => PlayerMove(4);
+        private void btnTake5_Click(object sender, EventArgs e) => PlayerMove(5);
+        private void btnTake6_Click(object sender, EventArgs e) => PlayerMove(6);
+        private void btnTake7_Click(object sender, EventArgs e) => PlayerMove(7);
+
+        private void lblRemainingDice_Click(object sender, EventArgs e) { }
+        private void lblTurn_Click(object sender, EventArgs e) { }
+        private void btnTake_Click(object sender, EventArgs e) { }
+        private void txtLog_TextChanged(object sender, EventArgs e) { }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    
+  
+}
